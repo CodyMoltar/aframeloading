14eb12d212b6a43685135b87f2c929b3796d72f3
CodyMoltar <66773011+CodyMoltar@users.noreply.github.com>
2022-11-13 15:40:36 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const video360 = document.getElementById("360video");
+
+
+// video360.addEventListener("playing", function() {
+//     console.log("[Playing] loading of video");
+//     if ( video360.readyState == 4 ) {
+//         console.log("[Finished] loading of video");
+//     }
+// });
+// video360.addEventListener("suspend", function(e) {
+//     console.log("[Suspended] loading of video");
+//     if ( video360.readyState == 4 ) {
+//         console.log("[Finished] loading of video");
+//     }
+// });
+
+// video360.addEventListener('progress', function() {
+//     let loadedPercentage = this.buffered.end(0) / this.duration;
+//     console.log(loadedPercentage);
+// });
